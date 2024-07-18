04cdabcb68dbb5fd1da902321b082ae602e7aa53
Kobi Samoray <kobi.samoray@broadcom.com>
2024-07-18 18:19:14 +0300

    Manual edits for TransitGatewayAttachment
    Also edits to the API wrapper-generated code, as it mishandles the case where the only implementation is multitenancy.
    
    Signed-off-by: Kobi Samoray <kobi.samoray@broadcom.com>

--- nsxt/resource_nsxt_policy_transit_gateway_attachment_test.go
+++ nsxt/resource_nsxt_policy_transit_gateway_attachment_test.go
@@ -5,6 +5,7 @@ package nsxt
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -12,15 +13,13 @@ import (
 )
 
 var accTestPolicyTransitGatewayAttachmentCreateAttributes = map[string]string{
-	"display_name":    getAccTestResourceName(),
-	"description":     "terraform created",
-	"connection_path": "test-create",
+	"display_name": getAccTestResourceName(),
+	"description":  "terraform created",
 }
 
 var accTestPolicyTransitGatewayAttachmentUpdateAttributes = map[string]string{
-	"display_name":    getAccTestResourceName(),
-	"description":     "terraform updated",
-	"connection_path": "test-update",
+	"display_name": getAccTestResourceName(),
+	"description":  "terraform updated",
 }
 
 func TestAccResourceNsxtPolicyTransitGatewayAttachment_basic(t *testing.T) {
@@ -39,7 +38,6 @@ func TestAccResourceNsxtPolicyTransitGatewayAttachment_basic(t *testing.T) {
 					testAccNsxtPolicyTransitGatewayAttachmentExists(accTestPolicyTransitGatewayAttachmentCreateAttributes["display_name"], testResourceName),
 					resource.TestCheckResourceAttr(testResourceName, "display_name", accTestPolicyTransitGatewayAttachmentCreateAttributes["display_name"]),
 					resource.TestCheckResourceAttr(testResourceName, "description", accTestPolicyTransitGatewayAttachmentCreateAttributes["description"]),
-					resource.TestCheckResourceAttr(testResourceName, "connection_path", accTestPolicyTransitGatewayAttachmentCreateAttributes["connection_path"]),
 
 					resource.TestCheckResourceAttrSet(testResourceName, "nsx_id"),
 					resource.TestCheckResourceAttrSet(testResourceName, "path"),
@@ -53,7 +51,6 @@ func TestAccResourceNsxtPolicyTransitGatewayAttachment_basic(t *testing.T) {
 					testAccNsxtPolicyTransitGatewayAttachmentExists(accTestPolicyTransitGatewayAttachmentUpdateAttributes["display_name"], testResourceName),
 					resource.TestCheckResourceAttr(testResourceName, "display_name", accTestPolicyTransitGatewayAttachmentUpdateAttributes["display_name"]),
 					resource.TestCheckResourceAttr(testResourceName, "description", accTestPolicyTransitGatewayAttachmentUpdateAttributes["description"]),
-					resource.TestCheckResourceAttr(testResourceName, "connection_path", accTestPolicyTransitGatewayAttachmentUpdateAttributes["connection_path"]),
 
 					resource.TestCheckResourceAttrSet(testResourceName, "nsx_id"),
 					resource.TestCheckResourceAttrSet(testResourceName, "path"),
@@ -114,7 +111,8 @@ func testAccNsxtPolicyTransitGatewayAttachmentExists(displayName string, resourc
 			return fmt.Errorf("Policy TransitGatewayAttachment resource ID not set in resources")
 		}
 
-		exists, err := resourceNsxtPolicyTransitGatewayAttachmentExists(resourceID, connector, testAccIsGlobalManager())
+		parentPath := rs.Primary.Attributes["parent_path"]
+		exists, err := resourceNsxtPolicyTransitGatewayAttachmentExists(testAccGetSessionContext(), parentPath, resourceID, connector)
 		if err != nil {
 			return err
 		}
@@ -135,7 +133,8 @@ func testAccNsxtPolicyTransitGatewayAttachmentCheckDestroy(state *terraform.Stat
 		}
 
 		resourceID := rs.Primary.Attributes["id"]
-		exists, err := resourceNsxtPolicyTransitGatewayAttachmentExists(resourceID, connector, testAccIsGlobalManager())
+		parentPath := rs.Primary.Attributes["parent_path"]
+		exists, err := resourceNsxtPolicyTransitGatewayAttachmentExists(testAccGetSessionContext(), parentPath, resourceID, connector)
 		if err == nil {
 			return err
 		}
@@ -154,22 +153,43 @@ func testAccNsxtPolicyTransitGatewayAttachmentTemplate(createFlow bool) string {
 	} else {
 		attrMap = accTestPolicyTransitGatewayAttachmentUpdateAttributes
 	}
-	return fmt.Sprintf(`
+	return testAccNsxtPolicyGatewayConnectionTemplate(true) + testAccNsxtVpcConnectivityProfilePrerequisite() + fmt.Sprintf(`
+data "nsxt_policy_project" "test" {
+  id = "%s"
+}
+
+resource "nsxt_policy_share" "share" {
+  display_name = "share"
+  shared_with  = [data.nsxt_policy_project.test.path]
+}
+
+resource "nsxt_policy_shared_resource" "test" {
+  share_path = nsxt_policy_share.share.path
+  display_name = "shared_resource"
+  resource_object {
+    resource_path = nsxt_policy_gateway_connection.test.path
+  }
+}
+
 resource "nsxt_policy_transit_gateway_attachment" "test" {
+  parent_path  = nsxt_transit_gateway.test.path
+  connection_path = nsxt_policy_gateway_connection.test.path
   display_name = "%s"
   description  = "%s"
-  connection_path = %s
 
   tag {
     scope = "scope1"
     tag   = "tag1"
   }
-}`, attrMap["display_name"], attrMap["description"], attrMap["connection_path"])
+
+  depends_on = [nsxt_policy_shared_resource.test]
+}`, os.Getenv("NSXT_PROJECT_ID"), attrMap["display_name"], attrMap["description"])
 }
 
 func testAccNsxtPolicyTransitGatewayAttachmentMinimalistic() string {
-	return fmt.Sprintf(`
+	return testAccNsxtVpcConnectivityProfilePrerequisite() + fmt.Sprintf(`
 resource "nsxt_policy_transit_gateway_attachment" "test" {
+  parent_path  = nsxt_transit_gateway.test.path
   display_name = "%s"
 
 }`, accTestPolicyTransitGatewayAttachmentUpdateAttributes["display_name"])
